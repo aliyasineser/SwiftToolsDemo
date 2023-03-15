f09b6b8c87d818647a4f968d42be7720c1024a4f
Ali Yasin Eser <aliyasineser@gmail.com>
2023-03-15 02:09:54 +0100

    Initial Commit

---
+++ SwiftToolsDemo/SwiftToolsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftToolsDemoApp.swift
+//  SwiftToolsDemo
+//
+//  Created by Ali Yasin Eser on 15.03.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftToolsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
