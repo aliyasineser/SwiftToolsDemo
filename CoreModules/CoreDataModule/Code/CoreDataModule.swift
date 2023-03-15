20d822d1addebe1ca98bfe83d8476e2d287f70f6
Ali Yasin Eser <aliyasineser@gmail.com>
2023-03-15 02:35:59 +0100

    Initial commit, has everything in need to start for running. Still, will add more targets on the main project

---
+++ CoreModules/CoreDataModule/Code/CoreDataModule.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataModule.swift
+//  CoreModules
+//
+//  Created by Ali Yasin Eser on 15.03.23.
+//
+
+import Foundation
+
+class CoreDataManager {
+    var id: String {
+        return "someID"}
+}
+
+// MARK: Test swiftlint
+extension CoreDataManager {
+    func test1(param:Int) {
+
+    }
+
+
+
+    func test2(param: Int) {
+
+
+
+    }
+
+    func test3(param :Int) {
+
+    }
+
+    func test4(param : Int) {
+
+    }
+
+    func test5(param: Int)
+    {
+
+    }
+}
---
+++ CoreModules/NetworkingModule/Code/Networking.swift
@@ -0,0 +1,37 @@
+//
+//  Networking.swift
+//  CoreModules
+//
+//  Created by Ali Yasin Eser on 15.03.23.
+//
+
+import Foundation
+
+class Networking {
+    var url: String {
+        return "someurl.invalid"}
+}
+
+// MARK: Test swiftlint
+extension Networking {
+    func test1(param:Int) {
+
+    }
+
+    func test2(param: Int) {
+
+    }
+
+    func test3(param :Int) {
+
+    }
+
+    func test4(param : Int) {
+
+    }
+
+    func test5(param: Int)
+    {
+
+    }
+}
